0a4383f349da7d016651649d8e62f2b196d6d064
Bin Budiman Affandi <b.binbudimanaffandi@dxc.com>
2023-10-20 11:43:47 +0800

    install Values library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Form from "./Form";
 import Colorlist from "./Colorlist";
+import Values from "values.js";
+import { useState } from "react";
 
 const App = () => {
+  const [colors, setColors] = useState('');
+
   return <main>
     <Form />
     <Colorlist />
